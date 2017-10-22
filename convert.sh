bb58fbc87458562aa648fbdd0f89aedf90c47daf
Brian Andress <opensourcefiend@gmail.com>
2017-10-21 21:46:37 -0400

    create filenames that are compatible with both windows and linux filesystems

--- convert.sh
+++ convert.sh
@@ -3,6 +3,19 @@
 # include the necessary amazon/audible information
 source ./config.sh
 
+universallyCompatibleFilename() {
+  shopt -s extglob;
+
+  local -r filename="$1"
+
+  # https://serverfault.com/a/776229
+  local compatible_filename=$(echo "$filename" | sed -e 's/[\\/:\*\?"<>\|\x01-\x1F\x7F]/ - /g' -e 's/^\(nul\|prn\|con\|lpt[0-9]\|com[0-9]\|aux\)\(\.\|$\)//i' -e 's/^\.*$//' -e 's/^$/NONAME/')
+
+  compatible_filename=$(echo "$compatible_filename" | sed -e 's/  / /g')
+
+  echo "$compatible_filename"
+}
+
 # go to the directory in which all of the downloaded aax files exist
 cd $AAX_DIR
 
@@ -11,7 +24,7 @@ for i in *.aax; do
   # construct the filename
   AUTHOR=$(ffmpeg -activation_bytes $ACTIVATION_BYTES -i "$i" 2>&1 | grep artist | head -1 | sed -e 's/    artist          : \(.*\)/\1/')
   TITLE=$(ffmpeg -activation_bytes $ACTIVATION_BYTES -i "$i" 2>&1 | grep title | head -1 | sed -e 's/    title           : \(.*\)/\1/')
-  FILENAME="$AUTHOR - $TITLE"
+  FILENAME=$(universallyCompatibleFilename "$AUTHOR - $TITLE")
 
   # save the cover art
   ffmpeg -activation_bytes $ACTIVATION_BYTES -i "$i" -c:v copy -an -sn "$COVER_ART_DIR/$FILENAME.jpg"
